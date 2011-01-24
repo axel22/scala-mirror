8cf3a7f8668e51cdf7f0ee0f14494cf48a07e423
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2011-01-24 17:27:01 +0100

    Several test changes.
    No review.

--- test/files/run/testblock.scala
+++ test/files/run/testblock.scala
@@ -10,7 +10,10 @@ import scala.parallel._
 object Test {
   
   def main(args: Array[String]) {
-    blockcomp(10)
+    if (util.Properties.isJavaAtLeast("1.6")) {
+      val vendor = util.Properties.javaVmVendor
+      if ((vendor contains "Sun") || (vendor contains "Apple")) blockcomp(10)
+    }
   }
   
   val lock = new java.util.concurrent.locks.ReentrantLock
